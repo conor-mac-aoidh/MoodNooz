5f0e95196e19e9defed7c38c74ec634fb4c3af12
Conor Mac Aoidh <conormacaoidh@gmail.com>
2013-04-22 19:29:21 +0100

    added design for UI

--- jsp/index.jsp
+++ jsp/index.jsp
@@ -1,6 +1,14 @@
-<%@include file="_header.jsp"%>
-<%
+<%@include file="header.jsp"%>
 
- out.write( "<h1>Hello World!</h1>" );
- 
- %>
+<div id="query">
+	<input type="text" name="Search" value="" style="width:65%"/>
+	<select name="when" style="width:20%">
+		<option>Today</option>
+		<option>This Week</option>
+		<option>This Month</option>
+		<option>This Year</option>
+	</select>
+	<input type="submit" name="go" value="Go" style="width:13%"/>
+</div>
+
+<%@include file="footer.jsp"%>
---
+++ jsp/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * General Javascript, script.js
+ * 
+ * @author     Conor Mac Aoidh <conormacaoidh@gmail.com>
+ */
+
+$(function(){
+	// show active page
+	var page = window.location.pathname.split( "/" ).pop( );
+	var el = $( "#menu ul li a[href=\"" + page + "\"]" );
+	if( el.length != 0 )
+		el.addClass( "current" ); 
+	else
+		$( "#menu ul li a:first" ).addClass( "current" );
+
+	// show row colors	
+	$( ".row-color tr:not(:hidden):even" ).removeClass( "odd" ).addClass( "even" );
+	$( ".row-color tr:not(:hidden):odd" ).removeClass( "even" ).addClass( "odd" );
+});
